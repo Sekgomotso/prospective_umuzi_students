8c5b17e372818c99663c6aadf4afd3636d0b123b
Sekgomotso <motsoshalang@gmail.com>
2020-01-28 18:00:37 +0200

    functions

--- students.js
+++ students.js
@@ -1,7 +1,3 @@
-
-// npm install --save pg
-// find out more here: https://node-postgres.com/
-
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "user",
@@ -27,3 +23,23 @@ const helloWorld = () => {
 
 helloWorld();
 
+const addNewVisitor = () => {
+  pool.query(
+    "INSERT INTO Visitor values($1::1, $2::'Sekgomotso',$3::30, $4::'17/09/2020',$5::'00:00', $6::'Tumi', $7::'nice')", 
+    ["1, 'Sekgomotso',30, '17/09/2020','00:00','Tumi', 'nice'"],
+    (err, data) => {
+      if (err) {
+        throw err;
+      }
+      console.log(data.rows);
+    }
+  );
+};
+
+addNewVisitor();
+
+const deleteVisitor = () => {
+  pool.query(
+    "DELETE FROM"
+  )
+}
