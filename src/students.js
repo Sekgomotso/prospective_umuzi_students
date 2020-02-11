5d4c33625dc27c52f0c00f1d11704a5934dc5ebd
Sekgomotso <motsoshalang@gmail.com>
2020-02-11 11:14:50 +0200

    working functions

--- src/students.js
+++ src/students.js
@@ -26,76 +26,77 @@ const helloWorld = () => {
 
 helloWorld();
 
-// Save visitor into database
-const addNewVisitor = async(name, age, date, time, nameOfAssistant, comment) => {
+// // Save visitor into database
+// const addNewVisitor = async(name, age, date, time, nameOfAssistant, comment) => {
 
-  var result;
+//   var result;
 
-  pool.query(
-    "INSERT INTO Visitors (visitor_name, visitors_age, date_of_visit, time_of_visit, assistant, comments) values ($1, $2, $3, $4, $5, $6)", 
-    [name, age, date, time, nameOfAssistant, comment],
-    async (err, data) => {
-      if (err) {
-        throw err;
-      }
+//   pool.query(
+//     "INSERT INTO Visitors (visitor_name, visitors_age, date_of_visit, time_of_visit, assistant, comments) values ($1, $2, $3, $4, $5, $6)", 
+//     [name, age, date, time, nameOfAssistant, comment],
+//     async (err, data) => {
+//       if (err) {
+//         throw err;
+//       }
 
-      result = await data;
+//       result = await data;
 
-    }
-  );
-  return await result;
-};
-let result = addNewVisitor('motso',30, '09/17/2020','00:00','Tumi', 'nice meeting you!')
-console.log(result)
+//     }
+//   );
+//   return await result;
+// };
+// let result = addNewVisitor('motso',30, '09/17/2020','00:00','Tumi', 'nice meeting you!')
+// console.log(result)
 
-// Return visitor's names and ID
-const visitorsIdName = () => {
-  pool.query(
-    "SELECT id, visitor_name FROM Visitors",
-    [], (err, data) => {
-      if (err) {
-        throw err;
-      }
-      console.log(data.rows);
-    }
-  );
-};
+// // Return visitor's names and ID
+// const visitorsIdName = () => {
+//   pool.query(
+//     "SELECT id, visitor_name FROM Visitors",
+//     [], (err, data) => {
+//       if (err) {
+//         throw err;
+//       }
+//       console.log(data.rows);
+//     }
+//   );
+// };
 
-visitorsIdName();
+// visitorsIdName();
 
-// Delete a visitor
-const deleteVisitor = () => {
-  pool.query(
-    "DELETE FROM Visitors WHERE $1", [visitor_name = 'motso'], (err, data) => {
-      if (err) {
-        throw err;
-      }
-      console.log(data.rows);
-    }
-  );
-};
+// // Delete a visitor
+// const deleteVisitor = () => {
+//   pool.query(
+//     "DELETE FROM Visitors WHERE id = $1", [35], (err, data) => {
+//       if (err) {
+//         throw err;
+//       }
+//       console.log(data.rows);
+//     }
+//   );
+// };
 
-deleteVisitor();
+// deleteVisitor();
 
-// Update a visitor
-const updateV = () => {
+// // Update a visitor
+// const updateV = () => {
 
-  pool.query(
-    "UPDATE Visitors", [visitor_name = 'juju'], (err, data) => {
-      if (err) {
-        throw err;
-      }
-      console.log(data.rows);
-    }
-  );
-};
+//   pool.query(
+//     "UPDATE Visitors SET visitor_name = $1", ['juju'], (err, data) => {
+//       if (err) {
+//         throw err;
+//       }
+//       console.log(data.rows);
+//     }
+//   );
+// };
 
-updateV();
+// updateV();
 
 // Return visitor's info given an id
 const visitorId = () => {
+  
   pool.query(
-    "SELECT * FROM Visitors WHERE $1", [1], (err, data) => {
+    "SELECT * FROM Visitors WHERE id = $1", [36], (err, data) => {
       if (err) {
         throw err;
       }
@@ -106,25 +107,25 @@ const visitorId = () => {
 
 visitorId();
 
-// Delete all visitors 
-const deleteAll = () => {
-  pool.query(
-    "DELETE FROM Visitors", [], (err, data) => {
-      if (err) {
-        throw err;
-      }
-      console.log(data.rows);
-    }
-  );
-};
+// // Delete all visitors 
+// const deleteAll = () => {
+//   pool.query(
+//     "DELETE FROM Visitors", [], (err, data) => {
+//       if (err) {
+//         throw err;
+//       }
+//       console.log(data.rows);
+//     }
+//   );
+// };
 
-deleteAll();
+// deleteAll();
 
-module.exports = {
-  addNewVisitor,
-  visitorsIdName,
-  deleteVisitor,
-  updateV,
-  visitorId,
-  deleteAll
-}
+// module.exports = {
+//   addNewVisitor,
+//   visitorsIdName,
+//   deleteVisitor,
+//   updateV,
+//   visitorId,
+//   deleteAll
+// }
