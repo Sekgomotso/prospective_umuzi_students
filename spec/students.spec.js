ef57a647be7bbc434540ab9211f7cecdc082665a
Sekgomotso <motsoshalang@gmail.com>
2020-02-11 17:33:40 +0200

    added tests

--- spec/students.spec.js
+++ spec/students.spec.js
@@ -1,7 +1,7 @@
 // Save visitor into database
 describe("sql and assignment", ()=>{
     const juju = require('../src/students');
-    it("saves vis..", async(err, data)=>{
+    it("save visitor into database", async(err, data)=>{
         
         let addNewVisitor = await juju ('motso',30, '09/17/2020','00:00','Tumi', 'nice meeting you!');
         expect(addNewVisitor[0]).toBe("motso")
@@ -11,11 +11,22 @@ describe("sql and assignment", ()=>{
 // Return Id and visitor's name
 describe("sql and assignment", ()=>{
     const juju = require('../src/students');
-    it("saves vis..", ()=>{
+    it("return Id and visitor's name", ()=>{
         
         let addNewVisitor = juju('motso',30, '09/17/2020','00:00','Tumi', 'nice meeting you!');
         
         expect(addNewVisitor.data.rows).toBe("id: 27, visitor_name: 'Sekgomotso'");
+    });    
+});
+
+// Update visitor
+describe("sql and assignment", ()=>{
+    const juju = require('../src/students');
+    it("update visitor", ()=>{
+        
+        let updateV = juju('motso',30, '09/17/2020','00:00','Tumi', 'nice meeting you!');
+        
+        expect(updateV.visitor_name).toBe("juju");
     });
     
 });
@@ -25,5 +36,5 @@ describe("node and sql assignment", ()=>{
     const db = require('../src/students');
     it('delete all visitors', ()=>{
         expect(db.addNewVisitor('motso',30, '09/17/2020','00:00','Tumi', 'nice meeting you!').toBe([]))
-    })
-})
+    });
+});
