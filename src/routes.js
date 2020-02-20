726bdb100dd8235a76d5ab4a76b9f9be1ec06484
Sekgomotso_s <motsoshalang@gmail.com>
2020-02-20 10:30:40 +0200

    adding to database trial

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false}));
+const path = require('path')
+
+const Pool = require("pg").Pool;
+
+require('dotenv').config();
 
 // form route
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/forms.html")
+    res.sendFile(path.join(__dirname + "/form.html"));
 });
 
-// submit button route
-app.post('/http://localhost:3000/new_visit', (req, res) => {
-    console.log("done");
-    return;
+// submit botton route
+app.post("/new_visit", (req, res) => {
+    addNewVisitor(req.body.name, req.body.assistant, req.body.age, req.body.date, req.body.time, req.body.comments);
+    res.sendFile(path.join(__dirname + "/form.html"));
 });
 
-app.listen(3000, ()=>{
-    console.log("finally")
+const addNewVisitor = async(name, nameOfAssistant, age, date, time, comment) => {
+
+    pool
+    .query(
+      "INSERT INTO Visitors (visitor_name, visitors_age, date_of_visit, time_of_visit, assistant, comments) values ($1, $2, $3, $4, $5, $6)", 
+      [name, nameOfAssistant, age, date, time, comment])
+    .then(data => (data.rows))
+    .catch(err => console.error("nope", err.stack))
+  };
+
+app.listen(3001, ()=>{
+    console.log("server running");
 });
