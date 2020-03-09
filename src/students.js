27ed5d60611a0d827d09b6963d39225540d89a62
Sekgomotso_s <motsoshalang@gmail.com>
2020-03-09 13:17:56 +0200

    added .env file

--- src/students.js
+++ src/students.js
@@ -3,30 +3,14 @@ const Pool = require("pg").Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-  user: "user",
-  host: "localhost",
-  database: "prospective_umuzi_students",
-  password: "pass",
-  port: 5432
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.DATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PORT
 });
 
 pool.connect();
-
-const helloWorld = () => {
-  pool.query(
-    "SELECT $1::text as message",
-    ["Hello world!"],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-
-      console.log(results.rows);
-    }
-  );
-};
-
-helloWorld(); 
 
 // Save visitor into database
 const addNewVisitor = async(name, age, date, time, nameOfAssistant, comment) => {
@@ -34,23 +18,18 @@ const addNewVisitor = async(name, age, date, time, nameOfAssistant, comment) => 
   try{
   
     query = await pool.query(
-      "INSERT INTO Visitors (visitor_name,                  visitors_age, date_of_visit, time_of_visit, assistant, comments) values ($1, $2, $3, $4, $5, $6)", 
+      "INSERT INTO Visitors (visitor_name, visitors_age, date_of_visit, time_of_visit, assistant, comments) values ($1, $2, $3, $4, $5, $6)", 
       [name, age, date, time, nameOfAssistant, comment]);
 
-      console.log(query.rows)
-      await pool.end()
-      // return query.rows
+      return query.rows
 
   } catch(err) {
     console.log(err)
 
-    await pool.end();
   }
 };
 
-addNewVisitor('Nothile', 25, '01/02/2020','14:00','Thami', 'Awesome.');
-
-// Return visitor's names and ID
+// Return an array of visitor's names and ID
 const visitorsIdName = async () => {
 
   try {
@@ -65,13 +44,11 @@ const visitorsIdName = async () => {
   }
 };
 
-visitorsIdName();
-
 // Delete a visitor
 const deleteVisitor = async () => {
   try {
     query = pool.query(
-      "DELETE FROM Visitors WHERE id = $1", [31]);
+      "DELETE FROM Visitors WHERE id = ${id}", [46]);
 
 
       return query.rows
@@ -82,15 +59,13 @@ const deleteVisitor = async () => {
   }
 };
 
-deleteVisitor();
-
 // Update a visitor
 const updateV = async () => {
 
   try {
 
     query = pool.query(
-      "UPDATE Visitors SET visitor_name = $1", ['Mbuso']);
+      "UPDATE Visitors SET visitor_name = ${visitor_name}", []);
 
       return query.rows;
 
@@ -100,15 +75,13 @@ const updateV = async () => {
 
 };
 
-updateV();
-
 // Return visitor's info given an id
 const visitorId = async () => {
 
   try {
     
     query = pool.query(
-      "SELECT * FROM Visitors WHERE id = $1", [32]);
+      "SELECT * FROM Visitors WHERE id = ${id}", []);
     
       return query.rows
 
@@ -118,8 +91,6 @@ const visitorId = async () => {
   
 };
 
-visitorId();
-
 // Delete all visitors 
 const deleteAll = async () => {
 
@@ -128,14 +99,14 @@ const deleteAll = async () => {
     query = pool.query(
       "DELETE FROM Visitors", []);
 
+      await pool.end()
       return query.rows
 
   } catch(err) {
     console.log(err)
+    await pool.end();
   }
 };
-
-deleteAll();
 
 module.exports = {
   addNewVisitor,
